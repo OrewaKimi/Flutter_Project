404e38d44ee19bb2dc4b3270e2028d5a2a77178e
Kimi Maulana <kimi93.com@gmail.com>
2025-03-16 15:13:18 +0700

    update feature switch and value slider

--- flutter_button/lib/button/cupertino_button.dart
+++ flutter_button/lib/button/cupertino_button.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
-class CupertinoButtonScreen extends StatelessWidget {
+class CupertinoButtonScreen extends StatefulWidget {
+  @override
+  _CupertinoButtonScreenState createState() => _CupertinoButtonScreenState();
+}
+
+class _CupertinoButtonScreenState extends State<CupertinoButtonScreen> {
+  int _sliderValue = 100;
+  bool _switchValue = false;
+
   void _showMessage(String message, BuildContext context) {
     showCupertinoDialog(
       context: context,
@@ -25,10 +33,41 @@ class CupertinoButtonScreen extends StatelessWidget {
         middle: const Text('Cupertino Button Demo'),
       ),
       child: Center(
-        child: CupertinoButton(
-          onPressed: () => _showMessage('Cupertino Button Clicked!', context),
-          color: CupertinoColors.activeBlue,
-          child: const Text('Cupertino Button'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoButton(
+              onPressed:
+                  () => _showMessage('Cupertino Button Clicked!', context),
+              color: CupertinoColors.activeBlue,
+              child: const Text('Cupertino Button'),
+            ),
+            SizedBox(height: 20),
+            CupertinoSlider(
+              value: _sliderValue.toDouble(),
+              min: 0,
+              max: 100,
+              onChanged: (value) {
+                setState(() {
+                  _sliderValue = value.toInt();
+                });
+              },
+            ),
+            Text('Slider Value: $_sliderValue', style: TextStyle(fontSize: 12)),
+            SizedBox(height: 20),
+            CupertinoSwitch(
+              value: _switchValue,
+              onChanged: (value) {
+                setState(() {
+                  _switchValue = value;
+                });
+              },
+            ),
+            Text(
+              _switchValue ? 'Switch On' : 'Switch Off',
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
         ),
       ),
     );
