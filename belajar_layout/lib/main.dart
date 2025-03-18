b4a5247fac0d17d181f9b059a3403afa1ea4abef
Kimi Maulana <kimi93.com@gmail.com>
2025-03-18 15:07:28 +0700

    add drawer

--- belajar_layout/lib/main.dart
+++ belajar_layout/lib/main.dart
@@ -11,53 +11,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ManageWidget(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class ManageWidget extends StatefulWidget {
-  const ManageWidget({super.key});
-
-  @override
-  _ManageWidgetState createState() => _ManageWidgetState();
-}
-
-class _ManageWidgetState extends State<ManageWidget> {
-  bool _isVisible = true;
-
-  void _toggleVisibility() {
-    setState(() {
-      _isVisible = !_isVisible;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Manage Widget")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: const Text('Layout Screen')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: [
-            AnimatedOpacity(
-              opacity: _isVisible ? 1.0 : 0.0,
-              duration: const Duration(milliseconds: 500),
-              child: Container(
-                width: 150,
-                height: 150,
-                color: Colors.green,
-                child: const Center(child: Text('Visible Widget')),
-              ),
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _toggleVisibility,
-              child: const Text('Toggle Visibility'),
+            ListTile(
+              title: const Text('Shoes'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Clothes'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Pants'),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
+      body: const Center(child: Text('Selamat datang di layout: scaffold!')),
     );
   }
 }
