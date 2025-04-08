c808b23fa6c6f9e0584512cdb7cda0a8860785fc
OrewaKimi <kimi93.com@gmail.com>
2025-04-08 16:31:30 +0700

    update code for pagetwo

--- belajar_navigasi/lib/home_with_drawer.dart
+++ belajar_navigasi/lib/home_with_drawer.dart
@@ -6,39 +6,11 @@ class HomeWithDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Drawer Navigation'),),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text('Drawer Header', style: TextStyle(color: Colors.white)),
-            ),
-              
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Settings Clicked')),
-                );
-                Navigator.pop(context); 
-              },
-            ),
-          ],
-        ),
-      ),
       body: Center(
-        child: Text('Welcome to Drawer App',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold) ),
+        child: Text(
+          'Welcome to Drawer App',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
